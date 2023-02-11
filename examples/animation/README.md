179c92bbcd230db29ddf15a6300f39fd38bd7d85
chensiyu <chensiyu@pjlab.org.cn>
2023-02-12 02:52:22 +0800

    rewrite animation with StableDiffusionImg2ImgPipeline

--- examples/animation/README.md
+++ examples/animation/README.md
@@ -2,21 +2,26 @@
 
 ## Setup
 
-1. Clone `git@github.com:google-research/frame-interpolation.git`, and then install requirements listed in `frame-interpolation/requirements.txt`.
+1. Clone `git@github.com:google-research/frame-interpolation.git` to this folder, and then install requirements listed in `frame-interpolation/requirements.txt`.
 ```
 git clone git@github.com:google-research/frame-interpolation.git
 pip install -r frame-interpolation/requirements.txt
 ```
 2. Download [`saved_model` folder](https://drive.google.com/drive/folders/1i9Go1YI2qiFWeT5QtywNFmYAA74bhXWj?usp=sharing) to this folder.
+3. Additional dependencies: `av`.
+```
+pip install av
+```
 
 ## Example
 
-Before running, `mkdir -p tmp`.
-
 ```
 python -u img2img_animation.py \
---prompt_start 'Gregory Manchess style, a kitchen, yellow wall, wooden floor, wooden table and cupboard, vases of flowers' \
---prompt_end 'Gregory Manchess style, rain forest, birds in trees | Gregory Manchess style, a city in the future, street view | Gregory Manchess style, a fantasy landscape, mountains, river' \
---init_image finalscene-impasto-extrastrength-blurred.jpg \
---width 1024 --height 576 --prompt_strength 0.9 --num_animation_frames 10 --guidance_scale 12 --film_interpolation --intermediate_output --seed 0
+--prompts 'Gregory Manchess style, a kitchen, yellow wall, wooden floor, wooden table and cupboard, vases of flowers' \
+'Gregory Manchess style, rain forest, birds in trees' \
+'Gregory Manchess style, a city in the future, street view' \
+'Gregory Manchess style, a fantasy landscape, mountains, river' \
+--init_image data/finalscene-impasto-extrastrength-blurred.jpg \
+--width 1024 --height 576 --strength 0.8 --num_animation_frames 10 --guidance_scale 5 --film_interpolation --seed 0 \
+--intermediate_output --output_path results
 ```
--- examples/animation/img2img_animation.py
+++ examples/animation/img2img_animation.py
@@ -1,18 +1,17 @@
 import argparse
+from datetime import datetime
 import sys
 import os
-from typing import Optional, List, Iterator
+from typing import List
 
 import cv2
 import av
 import numpy as np
 import torch
-from torch import autocast
 import tensorflow as tf
-from diffusers import PNDMScheduler, LMSDiscreteScheduler
 from PIL import Image
 
-from animate import StableDiffusionAnimationPipeline
+from diffusers import StableDiffusionImg2ImgPipeline
 
 sys.path.append("frame-interpolation")
 from eval import interpolator as film_interpolator, util as film_util
@@ -23,15 +22,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='runwayml/stable-diffusion-v1-5', help="Model id or path")
 # Start with either an image or a prompt
 parser.add_argument('--init_image', type=str, default=None, help="Image path to start the animation with")
-parser.add_argument('--prompt_start', type=str, default=None, help="Prompt to start the animation with")
-parser.add_argument(
-    '--prompt_end', type=str, default=None,
-    help="Prompt to end the animation with. You can include multiple prompts by separating the prompts with | (the 'pipe' character)")
+parser.add_argument('--prompts', type=str, nargs='+', help="Prompts for the animation")
 parser.add_argument('--width', type=int, default=512, help="Width of output image")
 parser.add_argument('--height', type=int, default=512, help="Height of output image")
 parser.add_argument('--num_inference_steps', type=int, default=50, help="Number of denoising steps (1~500)")
 parser.add_argument(
-    '--prompt_strength', type=float, default=0.8,
+    '--strength', type=float, default=0.8,
     help="Lower prompt strength generates more coherent gifs, higher respects prompts more but can be jumpy")
 parser.add_argument('--num_animation_frames', type=int, default=10, help="Number of frames to animate (2~50)")
 parser.add_argument(
@@ -50,18 +46,20 @@ parser.add_argument(
     help="Whether to display intermediate outputs during generation")
 parser.add_argument('--seed', type=int, default=None, help="Random seed. Leave blank to randomize the seed")
 parser.add_argument('--output_format', type=str, default="mp4", help="Output file format (gif or mp4)")
+parser.add_argument('--output_path', type=str, help="Directory for output files")
 
 
-class Predictor:
+class Img2ImgAnimation:
     def __init__(
-        self, model_id_or_path="runwayml/stable-diffusion-v1-5",
+        self, model, output_path,
         # from https://drive.google.com/drive/folders/1i9Go1YI2qiFWeT5QtywNFmYAA74bhXWj?usp=sharing
-        film_path="saved_model"
+        film_path="saved_model",
+        **kwargs
     ):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading pipeline...")
-        self.pipe = StableDiffusionAnimationPipeline.from_pretrained(
-            model_id_or_path,
+        self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+            model,
             torch_dtype=torch.float16,
             cache_dir=MODEL_CACHE,
             local_files_only=True,
@@ -78,17 +76,19 @@ class Predictor:
             None,
         )
 
-    @torch.inference_mode()
-    @torch.cuda.amp.autocast()
+        # Create output directory
+        self.output_path = output_path
+        os.makedirs(os.path.join(output_path, "tmp"), exist_ok=True)
+
+    @torch.no_grad()
     def predict(
         self,
         init_image: str,
-        prompt_start: str,
-        prompt_end: str,
+        prompts: List[str],
         width: int,
         height: int,
         num_inference_steps: int,
-        prompt_strength: float,
+        strength: float,
         num_animation_frames: int,
         num_interpolation_steps: int,
         guidance_scale: float,
@@ -97,164 +97,128 @@ class Predictor:
         film_interpolation: bool,
         intermediate_output: bool,
         seed: int,
-        output_format: str
+        output_format: str,
+        **kwargs
     ) -> str:
-        """Run a single prediction on the model"""
-        with torch.autocast("cuda"), torch.inference_mode():
-            if seed is None:
-                seed = int.from_bytes(os.urandom(2), "big")
-            print(f"Using seed: {seed}")
-            generator = torch.Generator("cuda").manual_seed(seed)
+        """Run multiple predictions on the model to form animation"""
+        # 1. Check inputs. Raise error if not correct
+        if not isinstance(prompts, list):
+            raise ValueError(f"`prompts` has to be of type `list` but is {type(prompts)}")
 
-            batch_size = 1
+        if strength < 0 or strength > 1:
+            raise ValueError(f"The value of strength should in [0.0, 1.0] but is {strength}")
 
-            # Generate initial latents to start to generate animation frames from
-            initial_scheduler = self.pipe.scheduler = make_scheduler(
-                num_inference_steps
+        # 2. Define call parameters
+        if seed is None:
+            seed = int.from_bytes(os.urandom(2), "big")
+        print(f"Using seed: {seed}")
+        batch_size = 1
+        num_images_per_prompt = 1
+        device = torch.device("cuda")
+        generator = torch.Generator(device).manual_seed(seed)
+        # here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
+        # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
+        # corresponds to doing no classifier free guidance.
+        do_classifier_free_guidance = guidance_scale > 1.0
+
+        # 3. Encode input prompt
+        prompt_embeds = [self.pipe._encode_prompt(
+            prompt,
+            device,
+            num_images_per_prompt,
+            do_classifier_free_guidance,
+            negative_prompt=None,
+        ) for prompt in prompts]
+
+        # 4. Preprocess image
+        image = load_img(init_image, (width, height))
+
+        # 5. set timesteps
+        self.pipe.scheduler.set_timesteps(num_inference_steps, device=device)
+        timesteps, num_denoise_steps = self.pipe.get_timesteps(num_inference_steps, strength, device)
+        latent_timestep = timesteps[:1].repeat(batch_size * num_images_per_prompt)
+        num_warmup_steps = len(timesteps) - num_denoise_steps * self.pipe.scheduler.order
+
+        # 6. Prepare latent variables
+        init_latents = self.pipe.prepare_latents(
+            image, latent_timestep, batch_size, num_images_per_prompt,
+            prompt_embeds[0].dtype, device, generator
+        )
+
+        # 7. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
+        extra_step_kwargs = self.pipe.prepare_extra_step_kwargs(generator, eta=0.)
+
+        # 8. Denoising loop
+        frames_latents = []
+        for prompt_idx in range(len(prompts) - 1):
+            for frame_idx in range(num_animation_frames):
+                print(f"Generating frame {frame_idx + 1} of prompt {prompt_idx}")
+                cur_prompt_embed = slerp(
+                    (frame_idx + 1) / num_animation_frames,
+                    prompt_embeds[prompt_idx],
+                    prompt_embeds[prompt_idx + 1],
+                )
+                latents = init_latents
+                with self.pipe.progress_bar(total=num_denoise_steps) as progress_bar:
+                    for i, t in enumerate(timesteps):
+                        # expand the latents if we are doing classifier free guidance
+                        latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+                        latent_model_input = self.pipe.scheduler.scale_model_input(latent_model_input, t)
+
+                        # predict the noise residual
+                        noise_pred = self.pipe.unet(latent_model_input, t, encoder_hidden_states=cur_prompt_embed).sample
+
+                        # perform guidance
+                        if do_classifier_free_guidance:
+                            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+                        # compute the previous noisy sample x_t -> x_t-1
+                        latents = self.pipe.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
+
+                        # call the callback, if provided
+                        if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.pipe.scheduler.order == 0):
+                            progress_bar.update()
+                frames_latents.append(latents)
+
+                # optionally save frames in separate files
+                if intermediate_output:
+                    image = self.pipe.decode_latents(latents)
+                    save_pil_image(
+                        self.pipe.numpy_to_pil(image)[0],
+                        path=os.path.join(
+                            self.output_path, "tmp",
+                            f"prompt_{prompt_idx}-frame_{frame_idx}.png"
+                        )
+                    )
+
+                # reset timesteps
+                self.pipe.scheduler.set_timesteps(num_inference_steps, device=device)
+
+        # 9. Post-processing
+        if gif_ping_pong:
+            frames_latents.extend(frames_latents[-2::-1])
+
+        # Decode images and interpolate between animation frames
+        if film_interpolation:
+            images = self.interpolate_film(frames_latents, num_interpolation_steps)
+        else:
+            images = self.interpolate_latents(
+                frames_latents, num_interpolation_steps
             )
 
-            num_initial_steps = int(num_inference_steps * (1 - prompt_strength))
-            do_classifier_free_guidance = guidance_scale > 1.0
+        # 10. Run safety checker
+        # image, has_nsfw_concept = self.pipe.run_safety_checker(images[0], device, prompt_embeds[0].dtype)
 
-            prompts = [prompt_start] + [
-                p.strip() for p in prompt_end.strip().split("|")
-            ]
-            keyframe_text_embeddings = []
-            for prompt in prompts:
-                keyframe_text_embeddings.append(
-                    self.pipe.embed_text(
-                        prompt, do_classifier_free_guidance, batch_size
-                    )
-                )
-
-            if init_image is not None:
-                print(f"Obtaining initial latents from {init_image}")
-                init_image = load_img(init_image, (width, height)).to("cuda")
-                latents_orig, latents_mid = self.pipe.latents_from_init_image(
-                    init_image,
-                    prompt_strength,
-                    num_inference_steps,
-                    batch_size,
-                    generator
-                )
-            else:
-                print(f"Generating initial latents for {num_initial_steps} steps")
-                initial_latents = torch.randn(
-                    (batch_size, self.pipe.unet.in_channels, height // 8, width // 8),
-                    generator=generator,
-                    device="cuda",
-                )
-
-                print("Generating first and last keyframes")
-
-                if len(prompts) % 2 == 0:
-                    i = len(prompts) // 2 - 1
-                    prev_text_emb = keyframe_text_embeddings[i]
-                    next_text_emb = keyframe_text_embeddings[i + 1]
-                    text_embeddings_mid = slerp(0.5, prev_text_emb, next_text_emb)
-                else:
-                    i = len(prompts) // 2
-                    text_embeddings_mid = keyframe_text_embeddings[i]
-
-                latents_mid = self.pipe.denoise(
-                    latents=initial_latents,
-                    text_embeddings=text_embeddings_mid,
-                    t_start=1,
-                    t_end=num_initial_steps,
-                    guidance_scale=guidance_scale,
-                )
-
-            keyframe_latents = []
-            for i in [0, -1]:
-                # re-initialize scheduler
-                self.pipe.scheduler = make_scheduler(num_inference_steps, initial_scheduler)
-                keyframe_latents.append(
-                    self.pipe.denoise(
-                        latents=latents_mid,
-                        text_embeddings=keyframe_text_embeddings[i],
-                        t_start=num_initial_steps,
-                        t_end=None,
-                        guidance_scale=guidance_scale,
-                    )
-                )
-
-            # image_start = self.pipe.latents_to_image(keyframe_latents[0])
-            # self.pipe.safety_check(image_start)
-
-            # image_end = self.pipe.latents_to_image(keyframe_latents[-1])
-            # elf.pipe.safety_check(image_end)
-
-            # Generate animation frames
-            frames_latents = []
-            # From initial image to starting prompt
-            if init_image is not None:
-                frames_latents = [latents_orig]
-                '''
-                latents_start = keyframe_latents[0]
-                for i in range(num_animation_frames):
-                    print(f"Generating frame {i + 1} of initial image")
-                    x = (i + 1) / num_animation_frames
-                    latents = latents_start * x + latents_orig * (1 - x)
-                    frames_latents.append(latents)
-                '''
-                if intermediate_output:
-                    for i, latents in enumerate(frames_latents):
-                        image = self.pipe.latents_to_image(latents)
-                        save_pil_image(
-                            self.pipe.numpy_to_pil(image)[0],
-                            path=f"tmp/output-init-{i}.png",
-                        )
-
-            for keyframe in range(len(prompts) - 1):
-                for i in range(num_animation_frames):
-                    print(f"Generating frame {i + 1} of keyframe {keyframe}")
-                    text_embeddings = slerp(
-                        (i + 1) / num_animation_frames,
-                        keyframe_text_embeddings[keyframe],
-                        keyframe_text_embeddings[keyframe + 1],
-                    )
-
-                    # re-initialize scheduler
-                    self.pipe.scheduler = make_scheduler(
-                        num_inference_steps, initial_scheduler
-                    )
-                    latents = self.pipe.denoise(
-                        latents=latents_mid,
-                        text_embeddings=text_embeddings,
-                        t_start=num_initial_steps,
-                        t_end=None,
-                        guidance_scale=guidance_scale,
-                    )
-
-                    # de-noise this frame
-                    frames_latents.append(latents)
-                    if intermediate_output:
-                        image = self.pipe.latents_to_image(latents)
-                        save_pil_image(
-                            self.pipe.numpy_to_pil(image)[0],
-                            path=f"tmp/output-{keyframe}-{i + 1}.png",
-                        )
-
-            if gif_ping_pong:
-                frames_latents.extend(frames_latents[-2::-1])
-
-            # Decode images by interpolate between animation frames
-            if film_interpolation:
-                images = self.interpolate_film(frames_latents, num_interpolation_steps)
-            else:
-                images = self.interpolate_latents(
-                    frames_latents, num_interpolation_steps
-                )
-
-            # Save the video
-            if output_format == "gif":
-                return self.save_gif(images, gif_frames_per_second)
-            else:
-                return self.save_mp4(images, gif_frames_per_second, width, height)
+        # Save the video
+        if output_format == "gif":
+            return self.save_gif(images, gif_frames_per_second)
+        else:
+            return self.save_mp4(images, gif_frames_per_second, width, height)
 
     def save_mp4(self, images, fps, width, height):
         print("Saving MP4")
-        output_path = "tmp/output.mp4"
+        output_path = os.path.join(self.output_path, f"output-{get_datetime()}.mp4")
 
         output = av.open(output_path, "w")
         stream = output.add_stream(
@@ -282,10 +246,10 @@ class Predictor:
     def save_gif(self, images, fps):
         print("Saving GIF")
         pil_images = [
-            self.pipe.numpy_to_pil(img.astype("float32"))[0] for img in images
+            self.pipe.numpy_to_pil(img)[0] for img in images
         ]
 
-        output_path = "tmp/video.gif"
+        output_path = os.path.join(self.output_path, f"output-{get_datetime()}.gif")
         gif_frame_duration = int(1000 / fps)
 
         with open(output_path, "wb") as f:
@@ -309,14 +273,14 @@ class Predictor:
             for j in range(num_interpolation_steps):
                 x = j / num_interpolation_steps
                 latents = latents_start * (1 - x) + latents_end * x
-                image = self.pipe.latents_to_image(latents)[0]
+                image = self.pipe.decode_latents(latents)[0]
                 images.append(image)
         return images
 
     def interpolate_film(self, frames_latents, num_interpolation_steps):
         print("Interpolating images with FILM")
         images = [
-            self.pipe.latents_to_image(lat)[0].astype("float32")
+            self.pipe.decode_latents(lat)[0].astype("float32")
             for lat in frames_latents
         ]
         if num_interpolation_steps == 0:
@@ -328,19 +292,6 @@ class Predictor:
         )
         images = [img.clip(0, 1) for img in images]
         return images
-
-
-def make_scheduler(num_inference_steps, from_scheduler=None):
-    scheduler = PNDMScheduler(
-        beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear"
-    )
-    scheduler.set_timesteps(num_inference_steps)
-    if from_scheduler:
-        scheduler.cur_model_output = from_scheduler.cur_model_output
-        scheduler.counter = from_scheduler.counter
-        scheduler.cur_sample = from_scheduler.cur_sample
-        scheduler.ets = from_scheduler.ets[:]
-    return scheduler
 
 
 def slerp(t, v0, v1, DOT_THRESHOLD=0.9995):
@@ -375,12 +326,11 @@ def load_img(path, resize=None):
     image = Image.open(path).convert("RGB")
     w, h = image.size
     print(f"loaded input image of size ({w}, {h}) from {path}")
-    if resize is None:
-        w, h = map(lambda x: x - x % 64, (w, h))  # resize to integer multiple of 64
-    else:
+    if resize is not None:
         w, h = resize
+    w, h = map(lambda x: x - x % 8, (w, h))  # resize to integer multiple of 8
     print(f"resizing to ({w}, {h})")
-    image = image.resize((w, h), resample=Image.LANCZOS)
+    image = image.resize((w, h), resample=Image.Resampling.LANCZOS)
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
@@ -391,9 +341,13 @@ def save_pil_image(image, path):
     image.save(path)
 
 
+def get_datetime():
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d-%H:%M:%S")
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     args = vars(args)
-    predictor = Predictor(args['model'])
-    args.pop('model')
+    predictor = Img2ImgAnimation(**args)
     print(predictor.predict(**args))
